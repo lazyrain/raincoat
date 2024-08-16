7b8b53045ef84a8be4211f14f0de38755cec96df
lazyrain <1635833+lazyrain@users.noreply.github.com>
2024-08-16 17:26:32 +0900

    パス起動処理の改造 (#15)
    * パス起動の処理を改造
    
    パイプ文字で区切ったパスを一度に起動できるように変更しました。
    
    * READMEに追記
    
    今回修正したパス起動についてのみ追記しました。
    
    ---------
    
    Co-authored-by: lazyrain <lazyrain@users.noreply.github.com>

--- raincoat/UseCases/Skills/StartProgram.cs
+++ raincoat/UseCases/Skills/StartProgram.cs
@@ -2,8 +2,21 @@
 
 namespace raincoat.UseCases.Skills
 {
+    /// <summary>
+    /// プログラムまたはフォルダを開始するためのユースケースを実装します。
+    /// </summary>
     public class StartProgram : IUseCase<SkillInputPack, SkillOutputPack>
     {
+        // パイプ文字をセパレータとして使用
+        private const string Separator = "|";
+
+        /// <summary>
+        /// 指定された引数に基づいてプログラムまたはフォルダを開始します。
+        /// </summary>
+        /// <param name="input">実行に必要な入力パラメータを含む <see cref="SkillInputPack"/> オブジェクト。</param>
+        /// <returns>実行結果を含む <see cref="SkillOutputPack"/> オブジェクト。</returns>
+        /// <exception cref="ArgumentNullException">入力が null または空の場合にスローされます。</exception>
+        /// <exception cref="FileNotFoundException">指定されたパスが存在しない場合にスローされます。</exception>
         public SkillOutputPack Execute(SkillInputPack input)
         {
 
@@ -12,8 +25,31 @@ namespace raincoat.UseCases.Skills
                 throw new ArgumentNullException(nameof(input), "パラメータの設定がおかしい。");
             }
 
-            var argumentPath = input.Argument;
+            string[] arguments;
+            if (input.Argument.Contains(Separator))
+            {
+                arguments = input.Argument.Split(new[] { Separator }, StringSplitOptions.RemoveEmptyEntries);
+            }
+            else
+            {
+                arguments = new[] { input.Argument };
+            }
 
+            foreach (var argumentPath in arguments.Select(arg => arg.Trim()))
+            {
+                this.ProcessArgument(argumentPath);
+            }
+
+            return new SkillOutputPack();
+        }
+
+        /// <summary>
+        /// 指定されたパスに基づいてプログラムまたはフォルダを開始します。
+        /// </summary>
+        /// <param name="argumentPath">開始するプログラムまたはフォルダのパス。</param>
+        /// <exception cref="FileNotFoundException">指定されたパスが存在しない場合にスローされます。</exception>
+        private void ProcessArgument(string argumentPath)
+        {
             if (File.Exists(argumentPath))
             {
                 Process.Start(argumentPath);
@@ -26,8 +62,6 @@ namespace raincoat.UseCases.Skills
             {
                 throw new FileNotFoundException("指定されたパスが見つかりません。", argumentPath);
             }
-
-            return new SkillOutputPack();
         }
     }
 }
